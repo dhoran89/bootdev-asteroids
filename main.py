7685c7941424226b7a477bce970d0b4b0129ed96
dhoran89 <dillon.horan@outlook.com>
2025-06-25 11:16:58 -0700

    add player update(dt) func, PLAYER_TURN_SPEED, call player(update)

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
 
 
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
 #create player
 class Player(CircleShape):
 
@@ -23,3 +24,14 @@ class Player(CircleShape):
         #draw player triangle (screen obj, color, list of points, line width)
         pygame.draw.polygon(screen,"white", self.triangle(), 2)
     
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        return self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
